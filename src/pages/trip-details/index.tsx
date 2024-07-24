3fd4ee3d9aec9ca908773b82855081e5b837b137
Jean Oliveira <jcwilbertt@gmail.com>
2024-07-24 23:59:40 +0100

    create-activity-modal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,18 @@
-import { Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCog, X } from "lucide-react";
+import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true)
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -30,7 +42,7 @@ export function TripDetailsPage() {
           {/*Div das atividades*/}
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 transition">
+            <button onClick={openCreateActivityModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 transition">
               <Plus className="size-5" />
               Cadastrar atividade
             </button>
@@ -82,27 +94,45 @@ export function TripDetailsPage() {
 
         {/*Parte lateral*/}
         <div className="w-80 space-y-6">
+          
+          <div className='w-full h-px bg-zinc-800' />
+          {/*Secção dos convidados*/}
           <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Links importantes</h2>
+            <h2 className="font-semibold text-xl">Convidados</h2>
             <div className="space-y-5">
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">Reserva do Airbnb</span>
-                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                    url do linkdjiasijhdasjidasijadsjidasjidsaijdijsadjiasdjisajidasijdjisadijsaidjasijdjsaidjisajdiasijdsajidsiajdiasji
-                  </a>
+                  <span className="block font-medium text-zinc-100">Jean Oliveira</span>
+                  <span className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                    jcwilbertt@gmail.com
+                  </span>
                 </div>
-                <Link2 className="text-zinc-400 size-5 shrink-0" />
+                <CircleDashed className="text-lime-400 size-5 shrink-0" />
+              </div>
+              {/*Segundo link começa aqui*/}
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Bárbara Pereira</span>
+                  <span className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                    barbarafjp12@gmail.com
+                  </span>
+                </div>
+                <CircleDashed className="text-lime-400 size-5 shrink-0" />
               </div>
             </div>
             <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 transition">
-              <Plus className="size-5" />
-              Cadastrar novo link
+              <UserCog className="size-5" />
+              Gerenciar convidados
             </button>
           </div>
-          <div className='w-full h-px bg-zinc-800' />
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal
+          closeCreateActivityModal={closeCreateActivityModal}
+        />
+      )}
     </div>
   );
 }
